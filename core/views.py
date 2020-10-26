45ea9d4adc143d984fe7e05b43f327e25bfcb248
thelilburna <72136386+thelilburna@users.noreply.github.com>
2020-10-26 12:40:05 +0000

    26.10.2020

--- core/views.py
+++ core/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def login_user(request):
     return render(request, 'login.html')
+
+def submit_login(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    print(username)
+    print(password)
--- sospet/urls.py
+++ sospet/urls.py
@@ -20,4 +20,5 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login_user)
+    path('login/submit', views.submit_login)
 ]
