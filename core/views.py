12a854329476498a3ca39bc629a9fe4afe5f2ced
thelilburna <72136386+thelilburna@users.noreply.github.com>
2020-11-02 12:37:46 +0000

    02.11.2020

--- core/views.py
+++ core/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def login_user(request):
     return render(request, 'login.html')
 
+@csrf_protect
 def submit_login(request):
     username = request.POST.get('username')
     password = request.POST.get('password')
-    print(username)
-    print(password)
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('/login/')
+    else:
+        message.error(request, 'Usuário e senha inválido. Favor tentar novamente.')
+        return redirect('/login/')
