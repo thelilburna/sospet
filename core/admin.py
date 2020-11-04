cf5e42f5ce82258eb388de7d2a82dda1f1ed7884
thelilburna <72136386+thelilburna@users.noreply.github.com>
2020-11-04 13:35:42 +0000

    04.11.2020

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Pet
+
 # Register your models here.
 
+#admin.site.register(PetLost)
 @admin.register(Pet)
 class PetAdmin(admin.ModelAdmin):
-    list_display = ['id', 'city', 'description', 'user']
-    
+    list_display = ['id', 'city', 'user', 'active',]
+    search_fields = ['id', 'user__username']
+
+
